b4d4d4aac11f9a26c7e265b4c522396211659fa9
Gabriel Silva <gabrielslvaz200@gmail.com>
2025-05-15 23:24:15 -0300

    Atualização da login.page.

--- lib/screens/login/login_page.dart
+++ lib/screens/login/login_page.dart
@@ -1,5 +1,22 @@
 import 'package:flutter/material.dart';
+void main() {
+  runApp(const MyApp());
+}
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const LoginPage(),
+    );
+  }
+}
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
